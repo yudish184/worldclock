39c9cabec3cbe3376d8c3f8b5a9f8c36974c81ca
Yudish Basnet <yudishbasnet1844@gmail.com>
2020-02-25 14:16:03 +0545

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/choose_location.dart';
+import 'package:flutter_app/pages/home.dart';
+import 'package:flutter_app/pages/loading.dart';
+
+void main() => runApp(MaterialApp(
+     
+     initialRoute: '/' ,
+      
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location':(context) => ChooseLocation(),
+      },
+    ));
+
+
+
